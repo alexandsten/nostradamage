416f4ac8c2f7fb99f8f546f86d6196d2768acb71
Alex A <alex.andsten@gmail.com>
2023-08-10 13:06:14 +0200

    start

---
+++ src/components/MovieAPI.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './Form'
+import MovieList from './MovieList'
+
+const MovieAPI = () => {
+  return (
+    <div>
+      <p>MovieAPI</p>
+      <Form />
+      <MovieList />
+    </div>
+  )
+}
+
+export default MovieAPI
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Movie from './Movie'
+
+const MovieList = () => {
+  return (
+    <div>
+      <p>Movielist</p>
+      <Movie />
+    </div>
+  )
+}
+
+export default MovieList
