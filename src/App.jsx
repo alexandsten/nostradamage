a9335d4c7bf446bfd071e381a68a326ee6eb3386
Alex A <alex.andsten@gmail.com>
2023-08-10 13:06:06 +0200

    start

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import MovieAPI from './components/MovieAPI'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <Header />
+      <MovieAPI />
+      <Footer />
       </>
   )
 }
