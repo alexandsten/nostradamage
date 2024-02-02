f6b0b33fabea832e8cfdbe629a55bf10cf3e88e6
Alex A <alex.andsten@gmail.com>
2024-02-02 02:07:08 +0100

    start

--- src/nostradamage/NostradamageView.tsx
+++ src/nostradamage/NostradamageView.tsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography';
 
 export default function NostradamageView() {
   return (
-      <Box>
+      <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', marginTop: '10%'}}>
         <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
             <Typography sx={{ fontSize: '22px', fontWeight: 'semiBold' }}>
                 Nostradamage
@@ -19,6 +19,17 @@ export default function NostradamageView() {
             <Typography sx={{ fontSize: '16px', fontWeight: 'semiBold', marginTop: '25px' }}>
                 Our product will allow the user to play around with their fantasy matchups and see our fight predictions.
             </Typography> 
+        </Box>
+
+        <Box sx={{ marginTop: '25px'}}>
+         <iframe
+            title="Video Player"
+            width="560"
+            height="315"
+            src={'https://packaged-media.redd.it/pks2bvh79tfc1/pb/m2-res_1920p.mp4?m=DASHPlaylist.mpd&v=1&e=1706853600&s=4559f86a7aef579621e9b723f2fb03ff82e0afad#t=0'}
+            frameBorder="0"
+            allowFullScreen
+        ></iframe>           
         </Box>
       </Box>
   )
