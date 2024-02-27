b70bd6cf9ee162f6be8f000c9d9c5deaac1ec896
Alex A <alex.andsten@gmail.com>
2024-02-27 23:48:40 +0100

    firebase start

---
+++ functions/myPets/index.js
@@ -0,0 +1,7 @@
+
+const functions = require('firebase-functions');
+
+exports.myPetsEndpoint = functions.https.onRequest((request, response) => {
+  const pets = [{ name: 'Fluffy', type: 'Cat' }, { name: 'Buddy', type: 'Dog' }];
+  response.json({ pets });
+});
