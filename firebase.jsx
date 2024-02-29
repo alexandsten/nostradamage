9156fa090059d575e2f16cd91201a993589d8c1a
Alex A <alex.andsten@gmail.com>
2024-02-29 23:52:53 +0100

    correct firebase config data

--- firebase.jsx
+++ firebase.jsx
@@ -1,14 +1,24 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-    const firebaseConfig = {
-        apiKey: 'YOUR_API_KEY',
-        authDomain: 'YOUR_AUTH_DOMAIN',
-        projectId: 'YOUR_PROJECT_ID',
-        storageBucket: 'YOUR_STORAGE_BUCKET',
-        messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-        appId: 'YOUR_APP_ID',
-      };
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBFK9TBeGAkS-5-9zOS4HJog4n_EfITLKI",
+
+  authDomain: "nostradamage-backend-01.firebaseapp.com",
+
+  projectId: "nostradamage-backend-01",
+
+  storageBucket: "nostradamage-backend-01.appspot.com",
+
+  messagingSenderId: "381854619282",
+
+  appId: "1:381854619282:web:2b2c211d9613baf52bf84b",
+
+  databaseURL: 'https://nostradamage-backend-01-default-rtdb.europe-west1.firebasedatabase.app/', 
+
+
+};
       
 
       const firebaseApp = initializeApp(firebaseConfig);
