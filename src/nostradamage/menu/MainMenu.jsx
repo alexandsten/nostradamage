a3f453dcbe79e715843405e9a92f35cf7ccd8553
Alex A <alex.andsten@gmail.com>
2024-03-14 21:28:03 +0100

    rework landing page

--- src/nostradamage/menu/MainMenu.jsx
+++ src/nostradamage/menu/MainMenu.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import { SocialIcon } from 'react-social-icons/component'
+import 'react-social-icons/instagram'
+import 'react-social-icons/discord'
+import 'react-social-icons/x'
+import 'react-social-icons/tiktok'
+
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" />
 
 
@@ -9,12 +15,24 @@ export default function MainMenu(props) {
   return (
     <Grid container spacing={2} sx={{ position: 'absolute', top: 0, width: '100%', padding: '20px', backgroundColor: 'white' }}>
       <Grid item xs={12} sm={1} sx={{ textAlign: 'center' }}>
-        <Typography sx={{ fontSize: '22px', fontWeight: 'Bold', fontFamily: "Roboto Mono" }}>
+        <Typography sx={{ fontSize: '22px', fontWeight: 'bold', fontFamily: "Roboto Mono" }}>
           Nostradamage
-        </Typography>
+        </Typography> 
       </Grid>
 
       <Grid container item xs={12} sm={11} spacing={2} justifyContent={'flex-end'}>
+      <Grid item >
+          <SocialIcon url="https://www.tiktok.com" />
+        </Grid>
+        <Grid item >
+          <SocialIcon url="https://www.instagram.com" />
+        </Grid>
+        <Grid item >
+          <SocialIcon url="https://www.x.com" />
+        </Grid>
+        <Grid item sx={{ marginRight: '5%'}}>
+          <SocialIcon url="https://www.discord.com" />
+        </Grid>
         <Grid item>
           <Typography
             sx={{ fontSize: '16px', fontWeight: 'semiBold', cursor: 'pointer', '&:hover': { textDecoration: 'underline' }, fontFamily: "Fira Sans" }}
