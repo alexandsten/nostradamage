7192cd9e8286fbdbc99b24d8b8a7c1b818349d02
Alex A <alex.andsten@gmail.com>
2024-03-21 21:24:21 +0100

    logo added

--- src/nostradamage/menu/MainMenu.jsx
+++ src/nostradamage/menu/MainMenu.jsx
@@ -6,6 +6,8 @@ import 'react-social-icons/instagram'
 import 'react-social-icons/discord'
 import 'react-social-icons/x'
 import 'react-social-icons/tiktok'
+import { Stack, Avatar } from '@mui/material';
+import nostraLogoHigh from '../img/nostraLogoHigh.png'
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" />
 
@@ -14,10 +16,12 @@ import 'react-social-icons/tiktok'
 export default function MainMenu(props) {
   return (
     <Grid container spacing={2} sx={{ position: 'sticky', top: 0, width: '101.5%', padding: '20px', backgroundColor: 'white', paddingRight: '50px' }}>
-      <Grid item xs={12} sm={1} sx={{ textAlign: 'center' }}>
+      <Grid item xs={12} sm={1} sx={{ textAlign: 'center', display: 'flex', flexDirection: 'row' }}>
+        <Avatar sx={{ width: '110px', height: '110px', marginTop: '-1.5em'}} src={nostraLogoHigh} />
         <Typography sx={{ fontSize: '22px', fontWeight: 'bold', fontFamily: "Roboto Mono" }}>
           Nostradamage
         </Typography> 
+        
       </Grid>
 
       <Grid container item xs={12} sm={11} spacing={2} justifyContent={'flex-end'}>
