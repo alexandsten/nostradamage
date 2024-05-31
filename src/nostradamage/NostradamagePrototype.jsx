541a6c6b098984e4a7083e52c4c19612bad319f4
Alex A <alex.andsten@gmail.com>
2024-05-31 14:23:02 +0200

    prototype rework start

--- src/nostradamage/NostradamagePrototype.jsx
+++ src/nostradamage/NostradamagePrototype.jsx
@@ -13,27 +13,18 @@ import { Stack, Avatar } from '@mui/material';
 
 export default function NostradamagePrototype() {
   const [data, setData] = useState([]);
-  const [toFetch, setToFetch] = useState(['UFC 302']);
+  const [toFetch, setToFetch] = useState(['UFC 300', 'UFC 301', 'UFC 302']);
   const [fighterNames, setFighterNames] = useState([]);
   const [fightButtonBool, setFightButtonBool] = useState(true);
 
   const firebaseConfig = {
-
     apiKey: "AIzaSyBFK9TBeGAkS-5-9zOS4HJog4n_EfITLKI",
-  
     authDomain: "nostradamage-backend-01.firebaseapp.com",
-  
     projectId: "nostradamage-backend-01",
-  
     storageBucket: "nostradamage-backend-01.appspot.com",
-  
     messagingSenderId: "381854619282",
-  
     appId: "1:381854619282:web:2b2c211d9613baf52bf84b",
-
     databaseURL: 'https://nostradamage-backend-01-default-rtdb.europe-west1.firebasedatabase.app/', 
-
-  
   };
 
   const firebaseApp = initializeApp(firebaseConfig);
@@ -45,7 +36,6 @@ export default function NostradamagePrototype() {
       try {
         await signInWithEmailAndPassword(auth, 'alex.andsten@gmail.com', 'jagvetiS11');
         console.log('User signed in successfully');
-        
       } catch (error) {
         console.error('Authentication error:', error);
       }
@@ -54,27 +44,17 @@ export default function NostradamagePrototype() {
     signInUser(); // Automatically sign in when the component mounts
   }, [auth]);
 
-  const onClickFighter = async () => {
-    console.log('click');
+  const onClickFighter = async (eventName) => {
+    console.log(eventName);
     try {
-      // Ensure the user is signed in before attempting to fetch data
+      const snapshot = await get(ref(database, eventName));
+      const fighterData = snapshot.val();
 
-      const fightersData = [];
-
-      for (const fighterName of toFetch) {
-        const snapshot = await get(ref(database, fighterName));
-        const fighterData = snapshot.val();
-
-        if (fighterData) {
-          fightersData.push(fighterData);
-        }
+      if (fighterData) {
+        setData((prevData) => [...prevData, fighterData]);
+        setFighterNames((prevNames) => [...prevNames, fighterData]);
+        setFightButtonBool(false);
       }
-
-      setData(fightersData);
-      setFighterNames(fightersData.map((fighter) => fighter));
-      // console.log(database);
-      // console.log(fighterNames);
-      setFightButtonBool(false)
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -97,68 +77,67 @@ export default function NostradamagePrototype() {
         <Stack direction='column'>
           <h2>Nostradamage prototype</h2>
           <p className='menu'>Click to open fight predictions</p>
-          <Accordion >
-            <AccordionSummary
-              expandIcon={<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 630 700"><path fill="currentColor" d="M622 106L311 417L0 106l65-65l246 245L556 41z"/></svg>}
-              aria-controls="panel1-content"
-              id="panel1-header"
-              onClick={onClickFighter}
-            >
-            <Typography sx={{ fontSize: '18px', fontWeight: 'semiBold', margin: '1em', fontFamily: "Roboto Mono", color: 'black' }}>
-              UFC 302
-            </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-            {!fightButtonBool && (
-              <Box
-                sx={{
-                  marginTop: '20px',
-                  marginBottom: '20px',
-                  justifyContent: 'center',
-                  display: 'flex',
-                  alignItems: 'center',
-                  flexDirection: 'column'
-                }}
+          {toFetch.map((event, index) => (
+            <Accordion key={index}>
+              <AccordionSummary
+                expandIcon={<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 630 700"><path fill="currentColor" d="M622 106L311 417L0 106l65-65l246 245L556 41z"/></svg>}
+                aria-controls={`panel${index}-content`}
+                id={`panel${index}-header`}
+                onClick={() => onClickFighter(event)}
               >
-              {fighterNames.length === 1 && (
-                    <Box>
-                        {Object.keys(fighterNames[0]).map((matchupName, index) => (
-                            <Box
-                                key={index}
-                                sx={{
-                                    backgroundColor: 'white',
-                                    borderRadius: '12px',
-                                    padding: '8px',
-                                    margin: '4px',
-                                    marginTop: '2em'
-                                }}
-                            >
-                                <div>
-                                    <b>
-                                      <p className='data'>
-                                        {matchupName}
-                                      </p>
-                                    </b>
-                            
+                <Typography sx={{ fontSize: '18px', fontWeight: 'semiBold', margin: '1em', fontFamily: "Roboto Mono", color: 'black' }}>
+                  {event}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                {!fightButtonBool && (
+                  <Box
+                    sx={{
+                      marginTop: '20px',
+                      marginBottom: '20px',
+                      justifyContent: 'center',
+                      display: 'flex',
+                      alignItems: 'center',
+                      flexDirection: 'column'
+                    }}
+                  >
+                    {fighterNames[index] && (
+                      <Box>
+                        {Object.keys(fighterNames[index]).map((matchupName, index2) => (
+                          <Box
+                            key={index2}
+                            sx={{
+                              backgroundColor: 'white',
+                              borderRadius: '12px',
+                              padding: '8px',
+                              margin: '4px',
+                              marginTop: '2em'
+                            }}
+                          >
+                            <div>
+                              <b>
+                                <p className='data'>
+                                  {matchupName}
+                                </p>
+                              </b>
+                            </div>
+                            <div>
+                              {Object.entries(fighterNames[index][matchupName]).map(([fighter, fighterData], idx) => (
+                                <div key={idx}>
+                                  <p className='data'>{fighter} {JSON.stringify(fighterData)}</p>
                                 </div>
-                                <div>
-                                    {Object.entries(fighterNames[0][matchupName]).map(([fighter, fighterData], idx) => (
-                                        <div  key={idx}>
-                                          <p className='data'>{fighter} {JSON.stringify(fighterData)}</p>
-                                            
-                                        </div>
-                                    ))}
-                                </div>
-                            </Box>
+                              ))}
+                            </div>
+                          </Box>
                         ))}
-                    </Box>
+                      </Box>
+                    )}
+                  </Box>
                 )}
-              </Box>
-            )}
-            </AccordionDetails>
-          </Accordion>
+              </AccordionDetails>
+            </Accordion>
+          ))}
         </Stack>
-
       </Box>
     </>
   );
