603c344bd79cc5475c07b268c80703161e5b3c26
Alex A <alex.andsten@gmail.com>
2024-06-08 01:41:49 +0200

    icon

--- src/nostradamage/NostradamageHome.jsx
+++ src/nostradamage/NostradamageHome.jsx
@@ -4,7 +4,7 @@ import { Typography, Stack } from '@mui/material';
 import '../../src/fonts/fonts.css';
 import './styles.css';
 import fighter1 from './img/fighter1.png';
-import machine1 from './img/machine1.png';
+import texture2 from './img/texture2.jpg';
 import static2 from './img/static2.webp';
 
 export default function NostradamageHome() {
@@ -25,7 +25,7 @@ export default function NostradamageHome() {
         <Stack
           sx={{
             width: '100vw',
-            height: '75vh'
+            height: '75vh',
           }}
           align="center"
           justifyContent="center"
@@ -40,8 +40,8 @@ export default function NostradamageHome() {
           mt={3}
           sx={{
             background: 'radial-gradient(circle, rgba(0, 0, 150, 1) 35%, rgba(0, 0, 90, 1) 40%, rgba(0, 0, 30, 1) 40%)',
-
-
+            backgroundImage: `linear-gradient(rgba(0, 0, 255, 0.7), rgba(0, 0, 0, 0.9)), url(${texture2})`,
+            backgroundSize: 'cover', 
             width: '100vw',
             height: '100vh'
           }}
