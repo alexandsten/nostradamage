0941c8c5d16bda1487236a5e92606e699d2126bd
Alex A <alex.andsten@gmail.com>
2024-06-08 16:49:25 +0200

    fix

--- src/nostradamage/NostradamageAbout.jsx
+++ src/nostradamage/NostradamageAbout.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { Grid, Stack } from '@mui/material';
 
 export default function NostradamageAbout() {
   return (
@@ -9,6 +10,7 @@ export default function NostradamageAbout() {
           sx={{
             display: 'flex',
             justifyContent: 'center',
+            alignItems: 'center',
             width: '100%',
             background: 'radial-gradient(circle, rgba(255,185,0,1) 0%, #ed652b 33%)',
             margin: '0px',
@@ -18,10 +20,43 @@ export default function NostradamageAbout() {
             minHeight: '80vh'
           }}
         >
-            <p style={{ fontSize: '24px'}}>
-                We are undefeated and undisputed
-            </p> 
+          <Grid 
+            container 
+            justifyContent='center' 
+            alignItems='center' 
+            spacing={4} 
+            sx={{ maxWidth: 'lg' }}
+          >
+            <Grid item xs={12} md={4} sx={{ display: 'flex', justifyContent: 'center' }}>
+              <Stack direction='column' alignItems='center'>
+                <img style={{ height: '250px', width: '200px', borderRadius: '10px'}} src='https://m.media-amazon.com/images/M/MV5BMjYxOTE3MzEyNF5BMl5BanBnXkFtZTYwNTM5MjUz._V1_FMjpg_UX1000_.jpg' />
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                  Jonathan
+                </Typography>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                  Co-creator
+                </Typography>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                  Data engineer / algorithm expert
+                </Typography>
+              </Stack>
+            </Grid>
+            <Grid item xs={12} md={4} sx={{ display: 'flex', justifyContent: 'center' }}>
+              <Stack direction='column' alignItems='center'>
+                <img style={{ height: '250px', width: '200px', borderRadius: '10px'}} src='https://m.media-amazon.com/images/M/MV5BMTc3NTM4MDQwNl5BMl5BanBnXkFtZTYwNDY0MjY1._V1_FMjpg_UX1000_.jpg' />
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                  Alex
+                </Typography>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                  Co-creator
+                </Typography>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                  Frontend / fullstack
+                </Typography>
+              </Stack>
+            </Grid>
+          </Grid>
         </Box>
     </>
-  )
+  );
 }
