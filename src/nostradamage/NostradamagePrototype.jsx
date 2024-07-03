137a3910f08241ed952fe408c0bf6cb716425f33
Alex A <alex.andsten@gmail.com>
2024-07-03 16:51:52 +0200

    fix

--- src/nostradamage/NostradamagePrototype.jsx
+++ src/nostradamage/NostradamagePrototype.jsx
@@ -82,138 +82,142 @@ export default function NostradamagePrototype() {
 
   return (
     <>
-      <Stack
-        sx={{
-          padding: '1em',
-          backgroundImage: `linear-gradient(rgba(0, 0, 150, 0.1), rgba(11, 21, 74, 0.6)), url(${monitorTexture})`,
-          backgroundSize: 'cover', 
-          border: '1px solid rgba(0, 0, 50, 1)', 
-          justifyContent: 'center',
-          alignItems: 'center',
-          minWidth: '50vw',
-          borderRadius: '25px',
-          margin: '2em',
-          marginTop: '0em'
-        }}
-      >
-       <Box
+      <Box sx={{ minHeight: '78vh'}}>
+        <Stack
           sx={{
-            display: 'flex',
-            backgroundImage: expanded 
-              ? `linear-gradient(rgba(0, 0, 30, 0.5), rgba(0, 0, 0, 0.9)), url(${pixelBackground1})` 
-              : `linear-gradient(rgba(0, 0, 30, 0.5), rgba(0, 0, 0, 0.9)), url(${pixelBackground2})`,
-            backgroundSize: 'cover', 
-            justifyContent: 'center',
-            width: '60vw',
-            borderRadius: '12px',
             padding: '1em',
-            marginTop: '3em',
-            marginBottom: '3em',
-            minHeight: '60vh',
-            minWidth: '30vw',
-            maxHeight: '50vh',
-            overflowY: 'auto', 
-            overflowX: 'hidden',
-            border: '8px solid black', 
+            backgroundImage: `linear-gradient(rgba(0, 0, 150, 0.1), rgba(11, 21, 74, 0.6)), url(${monitorTexture})`,
+            backgroundSize: 'cover', 
+            border: '1px solid rgba(0, 0, 50, 1)', 
+            justifyContent: 'center',
+            alignItems: 'center',
+            minWidth: '50vw',
+            borderRadius: '25px',
+            margin: '2em',
+            marginTop: '0em',
+            minHeight: '65vh',
+            maxHeight: '65vh'
           }}
         >
-   
-
-          
-          <Stack direction='column' justifyContent='center' alignItems='center' 
-            sx={{ height: '100%', borderRadius: '10px', width: '100%'
+        <Box
+            sx={{
+              display: 'flex',
+              backgroundImage: expanded 
+                ? `linear-gradient(rgba(0, 0, 30, 0.5), rgba(0, 0, 0, 0.9)), url(${pixelBackground1})` 
+                : `linear-gradient(rgba(0, 0, 30, 0.5), rgba(0, 0, 0, 0.9)), url(${pixelBackground2})`,
+              backgroundSize: 'cover', 
+              justifyContent: 'center',
+              width: '60vw',
+              borderRadius: '12px',
+              padding: '1em',
+              marginTop: '3em',
+              marginBottom: '3em',
+              minHeight: '50vh',
+              minWidth: '30vw',
+              maxHeight: '50vh',
+              overflowY: 'auto', 
+              overflowX: 'hidden',
+              border: '8px solid black', 
             }}
           >
-            <Stack direction='row' gap={2} alignItems='flex-start' justifyContent='center' m={3}>
-              <img style={{ height: '4em', width: '4em', borderRadius: '50%'}} src={nostraLogoColor}/> 
-              <h2>Nostradamage</h2>            
-            </Stack>
+    
+
             
-            {!expanded && <Typography className='data' style={{ color: 'white', fontSize: '20px', fontFamily: 'VT323'}}>Click to open fight predictions</Typography>}
-            {expanded ? (
-              <>
-                <Button variant="contained" onClick={resetView} sx={{ marginBottom: '1em', maxWidth: '18em', fontFamily: "Pixelify Sans", fontSize: '1.3em' }}>
-                  Reset
-                </Button>
-                {data[expanded] ? (
-                  <Box
-                    sx={{
-                      marginTop: '20px',
-                      marginBottom: '20px',
-                      justifyContent: 'center',
-                      display: 'flex',
-                      alignItems: 'center',
-                      flexDirection: 'column'
-                    }}
-                  >
+            <Stack direction='column' justifyContent='center' alignItems='center' 
+              sx={{ height: '100%', borderRadius: '10px', width: '100%'
+              }}
+            >
+              <Stack direction='row' gap={2} alignItems='flex-start' justifyContent='center' m={3}>
+                <img style={{ height: '4em', width: '4em', borderRadius: '50%'}} src={nostraLogoColor}/> 
+                <h2>Nostradamage</h2>            
+              </Stack>
+              
+              {!expanded && <Typography className='data' style={{ color: 'white', fontSize: '20px', fontFamily: 'VT323'}}>Click to open fight predictions</Typography>}
+              {expanded ? (
+                <>
+                  <Button variant="contained" onClick={resetView} sx={{ marginBottom: '1em', maxWidth: '18em', fontFamily: "Pixelify Sans", fontSize: '1.3em' }}>
+                    Reset
+                  </Button>
+                  {data[expanded] ? (
                     <Box
                       sx={{
-                        width: '40vw',
+                        marginTop: '20px',
+                        marginBottom: '20px',
+                        justifyContent: 'center',
                         display: 'flex',
-                        flexDirection: 'row',
-                        flexWrap: 'wrap', // Add flexWrap to allow items to wrap
-                        gap: '1em', // Add gap between items
+                        alignItems: 'center',
+                        flexDirection: 'column'
                       }}
                     >
-                      {Object.keys(data[expanded]).map((matchupName, index) => (
-                        <Box
-                          key={index}
-                          sx={{
-                            backgroundImage: `url(${staticBackground2})`,
-                            backgroundSize: 'cover', 
-                            padding: '8px',
-                            margin: '4px',
-                            marginTop: '2em',
-                            minWidth: '8em',
-                            maxWidth: '17em',
-                            flex: '1 1 30%',
-                            borderRadius: '15px',
-                            border: '3px solid white',
-                            transition: 'background-image 1.8s', // Add transition for smooth effect
-                            '&:hover': {
-                              backgroundImage: `url(${staticFilm})` // Darker color on hover
-                            },
-                          }}
-                        >
-                          <Stack>
-                              <Typography className='data' sx={{ fontSize: '18px', color: 'white', fontFamily: 'VT323' }}>
-                                {matchupName}
-                              </Typography>
-                          </Stack>
-                          <Stack>
-                            {Object.entries(data[expanded][matchupName]).map(([fighter, fighterData], idx) => (
-                              <Stack key={idx}>
-                                <Typography className='data' sx={{ fontSize: '20px', color: 'white', fontFamily: 'VT323' }}>{fighter} {JSON.stringify(fighterData)}</Typography>
-                              </Stack>
-                            ))}
-                          </Stack>
-                        </Box>
-                      ))}
+                      <Box
+                        sx={{
+                          width: '40vw',
+                          display: 'flex',
+                          flexDirection: 'row',
+                          flexWrap: 'wrap', // Add flexWrap to allow items to wrap
+                          gap: '1em', // Add gap between items
+                        }}
+                      >
+                        {Object.keys(data[expanded]).map((matchupName, index) => (
+                          <Box
+                            key={index}
+                            sx={{
+                              backgroundImage: `url(${staticBackground2})`,
+                              backgroundSize: 'cover', 
+                              padding: '8px',
+                              margin: '4px',
+                              marginTop: '2em',
+                              minWidth: '8em',
+                              maxWidth: '17em',
+                              flex: '1 1 30%',
+                              borderRadius: '15px',
+                              border: '3px solid white',
+                              transition: 'background-image 1.8s', // Add transition for smooth effect
+                              '&:hover': {
+                                backgroundImage: `url(${staticFilm})` // Darker color on hover
+                              },
+                            }}
+                          >
+                            <Stack>
+                                <Typography className='data' sx={{ fontSize: '18px', color: 'white', fontFamily: 'VT323' }}>
+                                  {matchupName}
+                                </Typography>
+                            </Stack>
+                            <Stack>
+                              {Object.entries(data[expanded][matchupName]).map(([fighter, fighterData], idx) => (
+                                <Stack key={idx}>
+                                  <Typography className='data' sx={{ fontSize: '20px', color: 'white', fontFamily: 'VT323' }}>{fighter} {JSON.stringify(fighterData)}</Typography>
+                                </Stack>
+                              ))}
+                            </Stack>
+                          </Box>
+                        ))}
+                      </Box>
                     </Box>
-                  </Box>
-                ) : (
-                  <Stack direction='row' justifyContent='center' alignItems='center' gap={1}>
-                    <Typography sx={{ fontSize: '2.4em', fontFamily: 'VT323' }}>Loading...</Typography>
-                    <img style={{ height: '4em', width: '4em', borderRadius: '50%'}} src={fight}/>
-                  </Stack>
-                  
-                )}
-              </>
-            ) : (
-              toFetch.map((event, index) => (
-                <Button
-                  key={index}
-                  variant="contained"
-                  onClick={() => handleButtonClick(event)}
-                  sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}
-                >
-                  {event}
-                </Button>
-              ))
-            )}
-          </Stack>
-        </Box>
-      </Stack>
+                  ) : (
+                    <Stack direction='row' justifyContent='center' alignItems='center' gap={1}>
+                      <Typography sx={{ fontSize: '2.4em', fontFamily: 'VT323' }}>Loading...</Typography>
+                      <img style={{ height: '4em', width: '4em', borderRadius: '50%'}} src={fight}/>
+                    </Stack>
+                    
+                  )}
+                </>
+              ) : (
+                toFetch.map((event, index) => (
+                  <Button
+                    key={index}
+                    variant="contained"
+                    onClick={() => handleButtonClick(event)}
+                    sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}
+                  >
+                    {event}
+                  </Button>
+                ))
+              )}
+            </Stack>
+          </Box>
+        </Stack>
+      </Box>
     </>
   );
 }
