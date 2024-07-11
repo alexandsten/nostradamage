42df72d6d734652c002e612838b190ddf02968e6
Alex A <alex.andsten@gmail.com>
2024-07-11 17:03:23 +0200

    fix

--- src/nostradamage/NostradamageHome.jsx
+++ src/nostradamage/NostradamageHome.jsx
@@ -86,12 +86,13 @@ export default function NostradamageHome({setView}) {
 
         <Stack
           mt={3}
+          pt={3}
           sx={{
             background: 'radial-gradient(circle, rgba(0, 0, 150, 1) 35%, rgba(0, 0, 90, 1) 40%, rgba(0, 0, 30, 1) 40%)',
             backgroundImage: `linear-gradient(rgba(0, 0, 255, 0.7), rgba(0, 0, 0, 0.9)), url(${texture2})`,
             backgroundSize: 'cover', 
             width: '100vw',
-            height: '100vh'
+            minHeight: '100vh'
           }}
           alignItems="center"
           justifyContent="center"
@@ -101,23 +102,26 @@ export default function NostradamageHome({setView}) {
           <h3>Our algorithm is always improving</h3>
           <Stack
             sx={{
-              height: '15em',
-              width: '50%',
-              backgroundColor: 'black',
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9)), url(${static2})`,
+              height: '35em',
+              width: '70%',
+              backgroundColor: 'white',
+              // backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.9)), url(${static2})`,
               backgroundSize: 'cover', 
               border: '8px solid black',
               borderRadius: '2em',
               marginTop: '2em'
             }}
-            p={5}
+            p={2}
             alignItems="center"
             justifyContent="center"
             direction="column"
           >
+           
+            <iframe title="netflix2" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiZTk2MzRmMzUtMzk3Yy00ZWZhLTlkMTktZjJkYWE5YTY0MDdlIiwidCI6ImY3NGUwNTA1LWMyODMtNDljZC04ODU5LWVkNDJjMmJmZDc5NCJ9" frameborder="0" allowFullScreen="true"></iframe>
+          </Stack>
+          <Stack m={3} justifyContent={'center'} alignItems={'center'}>
             <Typography sx={{fontSize: '1.3em', fontFamily: 'VT323' }}>66% success rate over the last 6 months</Typography>
             <Typography sx={{fontSize: '1.3em', fontFamily: 'VT323' }}>3/5 correct picks for the last main fight card</Typography>
-            <iframe title="netflix2" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZTk2MzRmMzUtMzk3Yy00ZWZhLTlkMTktZjJkYWE5YTY0MDdlIiwidCI6ImY3NGUwNTA1LWMyODMtNDljZC04ODU5LWVkNDJjMmJmZDc5NCJ9" frameborder="0" allowFullScreen="true"></iframe>
           </Stack>
         </Stack>
       </Box>
