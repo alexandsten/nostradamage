f1795cddcb90f49bb09c82ad2b47ec831df46722
Alex A <alex.andsten@gmail.com>
2024-07-13 08:37:12 +0200

    fix

--- src/nostradamage/NostradamageHome.jsx
+++ src/nostradamage/NostradamageHome.jsx
@@ -38,7 +38,7 @@ export default function NostradamageHome({setView}) {
     set([]);
     ref.current.push(setTimeout(() => set(['NOSTRA', 'PREDICTIONS', 'DAMAGE']), 2000));
     ref.current.push(setTimeout(() => set(['NOSTRA', 'DAMAGE']), 5000));
-    ref.current.push(setTimeout(() => set(['NOSTRA', 'DAMAGE', 'THE FUTURE OF PREDICTIONS - ']), 8000));
+    ref.current.push(setTimeout(() => set(['NOSTRADAMAGE', 'THE FUTURE', 'OF PREDICTIONS - ']), 8000));
   }, []);
 
   useEffect(() => {
