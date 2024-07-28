6803a39e60ce31cb95327cb9af45d9e5f0c4dc7f
Alex A <alex.andsten@gmail.com>
2024-07-28 08:29:39 +0200

    newlogo

--- src/nostradamage/NostradamagePrototype.jsx
+++ src/nostradamage/NostradamagePrototype.jsx
@@ -24,7 +24,7 @@ import styled, { keyframes } from 'styled-components';
 
 export default function NostradamagePrototype() {
   const [data, setData] = useState({});
-  const [toFetch, setToFetch] = useState(['UFC 302', 'UFC 303', 'UFC Fight Night Namajunas vs Cortez', 'UFC Fight Night Lemos vs Jandiroba']);
+  const [toFetch, setToFetch] = useState(['UFC 302', 'UFC 303', 'UFC 304', 'UFC Fight Night Namajunas vs Cortez', 'UFC Fight Night Lemos vs Jandiroba']);
   const [expanded, setExpanded] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
 
