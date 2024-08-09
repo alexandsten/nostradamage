5c87a17a5a57457b2cc9aef0f8af6c2f3516c070
Alex A <alex.andsten@gmail.com>
2024-08-09 11:35:27 +0200

    subscriptionstart

--- src/nostradamage/NostradamageSub.jsx
+++ src/nostradamage/NostradamageSub.jsx
@@ -29,51 +29,52 @@ export default function NostradamageDemo() {
             justifyContent='center' 
             alignItems='top' 
             spacing={4} 
+            gap={3}
             sx={{ maxWidth: 'lg' }}
           >
-            <Grid item xs={12} md={4} sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Grid item xs={12} md={4} sx={{ display: 'flex', justifyContent: 'center', backgroundColor: '#d4af37', borderRadius: '15px' }}>
               <Stack direction='column' alignItems='center'>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '22px', fontWeight: 'bold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Monthly membership
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
-                  Monthly fee
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans", color: 'black' }}>
+                  All inclusive - monthly fee
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'bold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Includes: 
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Predictions for early prelims 
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Predictions for prelims 
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Predictions for main card 
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Fighter data statistics 
                 </Typography>
               </Stack>
             </Grid>
-            <Grid item xs={12} md={4} sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Grid item xs={12} md={4} sx={{ display: 'flex', justifyContent: 'center', backgroundColor: '#C0C0C0', borderRadius: '15px' }}>
               <Stack direction='column' alignItems='center'>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '22px', fontWeight: 'bold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   One time fee
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
-                  Per fightcard
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans", color: 'black' }}>
+                  Pay per fightcard
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'bold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Includes: 
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Predictions for early prelims 
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Predictions for prelims 
                 </Typography>
-                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans" }}>
+                <Typography sx={{ margin: '1em', fontSize: '18px', fontWeight: 'semiBold', fontFamily: "Pixelify Sans", color: 'black' }}>
                   Predictions for main card 
                 </Typography>
               </Stack>
