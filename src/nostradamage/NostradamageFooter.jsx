fdd7e5764021b0aa890c0a8ca01f1e72c43fc603
Alex A <alex.andsten@gmail.com>
2024-11-14 14:40:18 +0100

    typography fix start

--- src/nostradamage/NostradamageFooter.jsx
+++ src/nostradamage/NostradamageFooter.jsx
@@ -3,9 +3,16 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import goldBg from './img/goldBg.jpg';
 import silverBg from './img/silverBg.jpg';
-import { Grid, Stack } from '@mui/material';
+import { Grid, Stack, styled as muiStyled } from '@mui/material';
 
 export default function NostradamageFooter() {
+
+  const Text = muiStyled(Typography)({
+    fontSize: '1.2em',
+    fontFamily: 'VT323',
+    color: 'white'
+  });
+
   return (
     <>
         <Stack
@@ -17,17 +24,17 @@ export default function NostradamageFooter() {
               boxShadow: '0 -4px 8px rgba(0, 0, 0, 0.2)',
               color: 'white',
               textAlign: 'center',
-              padding: '1em 0',
+              padding: '4em 0',
             }}
-            gap={1}
+            gap={5}
           >
-            <Typography variant="body2">
+            <Text>
               &copy; 2024 Nostradamage. All rights reserved.
-            </Typography>
-            <Typography variant="body2">
+            </Text>
+            <Text>
               Contact us: <a href="mailto:nostradmg@gmail.com" style={{ color: 'inherit' }}>nostradmg@gmail.com</a>
-            </Typography>
-            <Typography sx={{ color: 'white', fontFamily: "Gotu", fontSize: '9px' }}>
+            </Text>
+            <Typography sx={{ color: 'white', fontFamily: "VT323", fontSize: '0.8em' }}>
                 Background images Designed by Freepik, picture created by Craiyon, gifs by giphy.com
             </Typography>
           </Stack>
