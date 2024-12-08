ede6cbf15e26bf7f1245fe8edeb253389a814519
Alex A <alex.andsten@gmail.com>
2024-12-08 22:00:38 +0100

    new endpoint

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-const SUPABASE_URL = 'https://vjtqklqxzcqfrfenoziw.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqdHFrbHF4emNxZnJmZW5veml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkwMDMzMDMsImV4cCI6MjA0NDU3OTMwM30.aKOJes4BTZ0b_LUjHVqPe-53cZ3xjsWhVrZNeWOa8OE';  
+dotenv.config();
+
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+ 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
